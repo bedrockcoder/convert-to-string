2ff292b1d5304d0ecb4704a27ef1c33d126da254
BedrockCoder <bedrockcoder1@gmail.com>
2021-01-01 17:23:23 +0000

    Added options and updated types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,24 @@
+export interface Options {
+    /** 
+     * The default character to use for strings, e.g. `'Hello World'` uses `'single-quotes'`,
+     * `"Hello World"` uses `"double-quotes"`, and `` `Hello World` `` uses `` `backticks` ``
+     * @default 'single-quotes'
+     */
+    defaultString?: 'single-quotes' | 'double-quotes' | 'backticks';
+    /**
+     * How many spaces to use after a comma in arrays and objects, e.g. `1` space would be `['Hello', 'World']`,
+     * `0` spaces would be `['Hello','World']`, `3` spaces would be `['Hello',   'World']`, etc.
+     * @default 1
+     */
+    spacesAfterComma?: number;
+}
+
 /**
  * Converts a value of any data type to a string
- * @param obj The object to convert
+ * @param obj The object to convert to a string
+ * @param options The options to use when converting the object to a string
  * @returns The converted string
  */
-declare const convertToString: (obj?: any) => string;
+declare const convertToString: (obj?: unknown, options?: Options) => string;
 
 export = convertToString;
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,89 @@
-module.exports = (object?: any): string => {
-    if (typeof object === 'string') {
-        if (object.includes("'")) {
-            if (object.includes('"')) {
-                if (object.includes('`')) {
-                    return `'${object.replace(/'/g, "\\'")}'`;
-                }
-                return `\`${object}\``;
-            }
-            return `"${object}"`;
-        }
-        return `'${object}'`;
-    } else if (typeof object === 'number') {
-        return object.toString();
-    } else if (Array.isArray(object)) {
-        let str = '[';
-        object.forEach(element => {
-            str += `${module.exports(element)}, `;
-        });
-        return `${str.slice(0, -2)}]`;
-    } else if (object == null) {
-        return `${object}`;
-    } else if (typeof object === 'boolean') {
-        return object.toString();
-    } else if (typeof object === 'object') {
-        let str = '{ ';
-        Object.keys(object).forEach(key => {
-            const value = object[key];
-            str += `${key}: ${module.exports(value)}, `;
-        });
-        return `${str.slice(0, -2)} }`;
-    } else if (typeof object === 'function') {
-        const isAsync = object.toString().slice(0, 5) === 'async';
-        const isArrowFunction = object.toString().slice(isAsync ? 6 : 0, isAsync ? 14 : 8) !== 'function';
-        if (isArrowFunction) {
-            return object.toString();
-        } else {
-            return `${isAsync ? 'async ' : ''}function ${object.toString().slice(object.toString().indexOf('('))}`;
-        }
-    } else if (typeof object === 'symbol') {
-        return object.toString();
+import { Options } from '..';
+
+const generateSpace = (spaceCount: number) => {
+    let spaces = '';
+    for (let i = 0; i < spaceCount; i++) {
+        spaces += ' ';
     }
-    return '';
+    return spaces;
 };
+
+const convertToString = (obj?: unknown, options?: Options): string => {
+    if (!options) options = { defaultString: 'single-quotes', spacesAfterComma: 1 };
+    if (!options.defaultString) options.defaultString = 'single-quotes';
+    if (options.spacesAfterComma == null) options.spacesAfterComma = 1;
+    options.spacesAfterComma = Math.round(options.spacesAfterComma);
+    if (options.spacesAfterComma < 0) {
+        throw new Error('options.spacesAfterComma must be a positive integer');
+    }
+
+    if (typeof obj === 'string') {
+        if (options.defaultString === 'single-quotes') {
+            if (obj.includes("'")) {
+                if (obj.includes('"')) {
+                    if (obj.includes('`')) {
+                        return `'${obj.replace(/'/g, "\\'")}'`;
+                    }
+                    return `\`${obj}\``;
+                }
+                return `"${obj}"`;
+            }
+            return `'${obj}'`;
+        } else if (options.defaultString === 'double-quotes') {
+            if (obj.includes('"')) {
+                if (obj.includes("'")) {
+                    if (obj.includes('`')) {
+                        return `"${obj.replace(/"/g, '\\"')}"`;
+                    }
+                    return `\`${obj}\``;
+                }
+                return `'${obj}'`;
+            }
+            return `"${obj}"`;
+        } else {
+            if (obj.includes('`')) {
+                if (obj.includes("'")) {
+                    if (obj.includes('"')) {
+                        return `\`${obj.replace(/`/g, '\\`')}\``;
+                    }
+                    return `\"${obj}\"`;
+                }
+                return `'${obj}'`;
+            }
+            return `\`${obj}\``;
+        }
+    } else if (typeof obj === 'number') {
+        return obj.toString();
+    } else if (Array.isArray(obj)) {
+        let str = '[';
+        obj.forEach(element => {
+            str += `${convertToString(element, options)},${generateSpace(<number>options!.spacesAfterComma)}`;
+        });
+        return `${str.slice(0, (options.spacesAfterComma + 1) * -1)}]`;
+    } else if (obj == null) {
+        return `${obj}`;
+    } else if (typeof obj === 'boolean') {
+        return obj.toString();
+    } else if (typeof obj === 'object') {
+        let str = '{ ';
+        Object.keys(<{ [key: string]: unknown; }>obj).forEach(key => {
+            const value = (<{ [key: string]: unknown; }>obj)[key];
+            str += `${key}: ${convertToString(value, options)},${generateSpace(<number>options!.spacesAfterComma)}`;
+        });
+        return `${str.slice(0, (options.spacesAfterComma + 1) * -1)} }`;
+    } else if (typeof obj === 'function') {
+        const isAsync = obj.toString().slice(0, 5) === 'async';
+        const isArrowFunction = obj.toString().slice(isAsync ? 6 : 0, isAsync ? 14 : 8) !== 'function';
+        if (isArrowFunction) {
+            return obj.toString();
+        } else {
+            return `${isAsync ? 'async ' : ''}function ${obj.toString().slice(obj.toString().indexOf('('))}`;
+        }
+    } else {
+        return (<symbol>obj).toString();
+    }
+};
+
+if (typeof window === 'undefined') {
+    module.exports = convertToString;
+}
